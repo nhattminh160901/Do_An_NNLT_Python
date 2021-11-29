2fc8589bc8b6d27867237641da1ce431e1d6c0f1
nhattminh160901 <72496975+nhattminh160901@users.noreply.github.com>
2021-11-29 11:34:07 +0700

    Update testmore.py

--- testmore.py
+++ testmore.py
@@ -1,12 +1,4 @@
-from allclass import Lecturer
-from allclass import Student
-from allclass import StudyPoint
-from allclass import TrainingPoint
-from allclass import Rank
-from allclass import Course
-from allclass import Specialization
-from allclass import Union
-from allclass import Club
+from allclass import *
 
 student = Student('Le Quang Nhat', 'Minh', 21, 'male', '0898989898', 'minh@gmail.com', '20E1020074')
 cs = Course('Engineer', student)
